dec67764654e73e49f58882950cb137f22e9f269
julianolorenzato <julianolorenzato@gmail.com>
2024-12-20 20:12:19 -0300

    new: nvim initial files

---
+++ .config/nvim/lua/core/options.lua
@@ -0,0 +1,27 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & identation
+opt.tabstop = 2 -- 2 spaces for tabs
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = false
+
+-- search
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearence
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
